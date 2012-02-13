b092a3cabd11353fd569340267e2d7dda6ed2a1f
Brendan Forster <me@brendanforster.com>
2012-02-13 22:35:28 +1100

    some additional files it seems

--- build.cmd
+++ build.cmd
@@ -1,18 +1,7 @@
 @echo Off
 set config=%1
 if "%config%" == "" (
-   set config=debug
+   set config=Debug
 )
 
-:: compile the code
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src/SomeProject.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-
-:: remove all obj folder contents
-for /D %%f in (".\tests\*") do @(
-del /S /Q "%%f\obj\*"
-)
-
-:: find all test files and run them
-for /R %%F in (*Tests.dll) do (
-.\tools\xunit\xunit.console.clr4.exe %%F
-)
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /p:Configuration="%config%" /t:AppHarbor /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
