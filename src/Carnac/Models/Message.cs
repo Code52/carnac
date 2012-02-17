8b14d47af468325744daf092042734548ae66713
aeoth <paul@theleagueofpaul.com>
2012-02-18 10:52:24 +1100

    "Modifier" keys (And some others) now use a keycap font to stand out. This required some refactoring, Message (model) now has ObservableCollection<string> instead of just <string>

--- src/Carnac/Models/Message.cs
+++ src/Carnac/Models/Message.cs
@@ -1,18 +1,20 @@
 using System;
+using System.Collections.ObjectModel;
 using Caliburn.Micro;
-using Carnac.Logic;
 
 namespace Carnac.Models
 {
     public class Message: PropertyChangedBase
     {
+        public Message()
+        {
+            Text = new ObservableCollection<string>();
+        }
         public string ProcessName { get; set; }
 
         public DateTime StartingTime { get; set; }
         public DateTime LastMessage { get; set; }
-
-        [NotifyProperty(AlsoNotifyFor = new[] {"Count", "LastMessage"})]
-        public string Text { get; set; }
+        public ObservableCollection<string> Text { get; set; }
         public int Count { get; set; }
 
         public bool IsDeleting { get; set; }
