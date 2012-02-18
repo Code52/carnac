2c8f75b4a51c7c0ddc1c43404b2e865965d37c7c
Andrew Tobin <andrew@grrargh.com>
2012-02-19 02:40:55 +1100

    So, can set the main window as topmost but then NONE OF THE BUTTONS ON THE OTHER WINDOW WORK.
    Kind of a tradeoff I guess *sigh*

--- src/Carnac/Views/KeyShowView.xaml.cs
+++ src/Carnac/Views/KeyShowView.xaml.cs
@@ -21,6 +21,84 @@ namespace Carnac.Views
             base.OnSourceInitialized(e);
             var hwnd = new WindowInteropHelper(this).Handle;
             Win32Methods.SetWindowExTransparent(hwnd);
+
+            var timer = new Timer(100);
+            timer.Elapsed +=
+                (s, x) =>
+                {
+                    if (Application.Current == null || Application.Current.Dispatcher == null) return;
+
+                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                    {
+                        RECT rect;
+
+                        if (GetWindowRect(hwnd, out rect))
+                        {
+                            SetWindowPos(hwnd, HWND.TOPMOST, rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top, 0);
+                        }
+                    }));
+                };
+
+            timer.Start();
+        }
+
+        //[DllImport("user32.dll", SetLastError = true)]
+        //static extern bool BringWindowToTop(IntPtr hWnd);
+
+        //[DllImport("user32.dll")]
+        //private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int W, int H, uint uFlags);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        /// <summary>
+        /// HWND values for hWndInsertAfter
+        /// </summary>
+        public static class HWND
+        {
+            public static readonly IntPtr
+            NOTOPMOST = new IntPtr(-2),
+            BROADCAST = new IntPtr(0xffff),
+            TOPMOST = new IntPtr(-1),
+            TOP = new IntPtr(0),
+            BOTTOM = new IntPtr(1);
+        }
+
+
+        /// <summary>
+        /// SetWindowPos Flags
+        /// </summary>
+        public static class SWP
+        {
+            public static readonly int
+            NOSIZE = 0x0001,
+            NOMOVE = 0x0002,
+            NOZORDER = 0x0004,
+            NOREDRAW = 0x0008,
+            NOACTIVATE = 0x0010,
+            DRAWFRAME = 0x0020,
+            FRAMECHANGED = 0x0020,
+            SHOWWINDOW = 0x0040,
+            HIDEWINDOW = 0x0080,
+            NOCOPYBITS = 0x0100,
+            NOOWNERZORDER = 0x0200,
+            NOREPOSITION = 0x0200,
+            NOSENDCHANGING = 0x0400,
+            DEFERERASE = 0x2000,
+            ASYNCWINDOWPOS = 0x4000;
         }
     }
 }
