b5485c64a3b340d59e63aefdcb50887db5b7a45b
Brendan Forster <me@brendanforster.com>
2012-02-18 17:44:21 +1100

    introduced screen manager and extracted behaviour from ShellViewModel. Updated bootstrapper as it only registers application assembly

---
+++ src/Carnac.Logic/IScreenManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Carnac.Logic.Native;
+
+namespace Carnac.Logic
+{
+    [InheritedExport]
+    public interface IScreenManager
+    {
+        IEnumerable<DetailedScreen> GetScreens();
+    }
+}
