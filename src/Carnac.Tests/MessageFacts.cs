d4b8d6b5d628acfdd7351b7b999a37b31a14bae8
Jake Ginnivan <jake.ginnivan@readify.net>
2012-02-19 13:58:37 +0800

    Added test for grouping back messages

---
+++ src/Carnac.Tests/MessageFacts.cs
@@ -0,0 +1,22 @@
+﻿using Carnac.Models;
+using Xunit;
+
+namespace Carnac.Tests
+{
+    public class MessageFacts
+    {
+        [Fact]
+        public void message_groups_multiple_backspace_key_presses_together()
+        {
+            // arrange
+            var message = new Message();
+            message.AddText("Back");
+
+            // act
+            message.AddText("Back");
+
+            // assert
+            Assert.Equal("Back x 2 ", string.Join(string.Empty, message.Text));
+        }
+    }
+}
