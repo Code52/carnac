66ae5fa132c5463ff873a33de4df56192f454255
Jake Ginnivan <jake@ginnivan.net>
2015-01-31 20:09:07 +0000

    Cleanup preferences view xaml

---
+++ src/Carnac/UI/PreferencesField.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Carnac.UI
+{
+    public class PreferencesField : ContentControl
+    {
+        static PreferencesField()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PreferencesField), new FrameworkPropertyMetadata(typeof(PreferencesField)));
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+            "Header", typeof (string), typeof (PreferencesField), new PropertyMetadata(default(string)));
+
+        public string Header
+        {
+            get { return (string) GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty SecondaryControlProperty = DependencyProperty.Register(
+            "SecondaryControl", typeof (object), typeof (PreferencesField), new PropertyMetadata(default(object)));
+
+        public object SecondaryControl
+        {
+            get { return GetValue(SecondaryControlProperty); }
+            set { SetValue(SecondaryControlProperty, value); }
+        }
+    }
+}
