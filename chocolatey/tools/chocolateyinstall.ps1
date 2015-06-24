8da33ac7876b20897cab82e6ef6b83f36b84d856
Dustin Venegas <dustin.venegas@gmail.com>
2015-06-24 00:07:30 -0700

    Add a general chocolatey nuspec
    This package will perform a bin download and install of carnac,
    including carnac.exe in the path (through choco bin), and create a user
    start menu entry.

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'carnac'
+$url = 'https://github.com/downloads/Code52/carnac/Carnac.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Include Carnac.exe as a GUI in the bin install and ignore vshost.exe
+New-Item -ItemType file -force -path "$toolsDir\Carnac\Carnac.exe.gui" | out-null
+New-Item -ItemType file -force -path "$toolsDir\Carnac\Carnac.vshost.exe.ignore" | out-null
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir"
+
+# Create User start menu link
+$startMenuLink=$("$env:appdata\Microsoft\Windows\Start Menu\Programs\Carnac.lnk")
+Install-ChocolateyShortcut -shortcutFilePath $startMenuLink -targetPath "$toolsDir\Carnac\Carnac.exe" | out-null
