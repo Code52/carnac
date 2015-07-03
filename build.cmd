fc84eb95ad0c7bb02734a307a5ec0c2675eb4872
Dustin Venegas <dustin.venegas@gmail.com>
2015-07-03 14:31:39 -0700

    Build add version variable to build.cmd

--- build.cmd
+++ build.cmd
@@ -4,4 +4,9 @@ if "%config%" == "" (
    set config=Debug
 )
 
+set version=%2
+if "%version%" == "" (
+   set version=%config%
+)
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /p:Configuration="%config%" /t:package /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
