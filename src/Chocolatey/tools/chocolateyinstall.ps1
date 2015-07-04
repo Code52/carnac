5ac2771d3fc9ee63ea0b4efe1afc5770fbcc27c3
Dustin Venegas <dustin.venegas@gmail.com>
2015-07-04 12:00:22 -0700

    Choco - Moved Carnac bin deploy location
    The zip I originally used unpacked to a different location.

--- src/Chocolatey/tools/chocolateyinstall.ps1
+++ src/Chocolatey/tools/chocolateyinstall.ps1
@@ -1,14 +1,15 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $packageName = 'carnac'
 $url = 'Download Url Here'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir = "$toolsDir\Carnac"
 
 # Include Carnac.exe as a GUI in the bin install and ignore vshost.exe
-New-Item -ItemType file -force -path "$toolsDir\Carnac\Carnac.exe.gui" | out-null
-New-Item -ItemType file -force -path "$toolsDir\Carnac\Carnac.vshost.exe.ignore" | out-null
+New-Item -ItemType file -force -path "$installDir\Carnac.exe.gui" | out-null
+New-Item -ItemType file -force -path "$installDir\Carnac.vshost.exe.ignore" | out-null
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir"
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
 
 # Create User start menu link
 $startMenuLink=$("$env:appdata\Microsoft\Windows\Start Menu\Programs\Carnac.lnk")
-Install-ChocolateyShortcut -shortcutFilePath $startMenuLink -targetPath "$toolsDir\Carnac\Carnac.exe" | out-null
+Install-ChocolateyShortcut -shortcutFilePath $startMenuLink -targetPath "$installDir\Carnac.exe" | out-null
