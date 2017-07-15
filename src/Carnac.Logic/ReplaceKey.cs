eabc1217fb1ad8f8842308b97c7e720e32d8ce2b
Boris Fritscher <boris@fritscher.ch>
2017-07-15 21:10:42 +0200

    Support to show  keyboard keys based on locales

--- src/Carnac.Logic/ReplaceKey.cs
+++ src/Carnac.Logic/ReplaceKey.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace Carnac.Logic
 {
@@ -84,6 +87,7 @@ namespace Carnac.Logic
             {Keys.LMenu, "Alt"},
         };
 
+        // kept to continue to support keymaps parsing
         public static Keys? ToKey(string keyText)
         {
             foreach (var shiftReplacement in ShiftReplacements)
@@ -103,21 +107,51 @@ namespace Carnac.Logic
             return null;
         }
 
-        public static string Sanitise(this Keys key)
+        // new implementation of sanitize to support locals
+        // https://stackoverflow.com/questions/318777/c-sharp-how-to-translate-virtual-keycode-to-char
+        static public string KeyCodeToUnicode(Keys key, bool lowerOnly = false)
         {
-            return Replacements.ContainsKey(key) ? Replacements[key] : string.Format(key.ToString());
-        }
-
-        public static bool SanitiseShift(this Keys key, out string sanitisedKeyInput)
-        {
-            if (ShiftReplacements.ContainsKey(key))
+            byte[] keyboardState = new byte[255];
+            if (!lowerOnly)
             {
-                sanitisedKeyInput = ShiftReplacements[key];
-                return true;
+                bool keyboardStateStatus = GetKeyboardState(keyboardState);
+                if (!keyboardStateStatus)
+                {
+                    return "";
+                }
             }
 
-            sanitisedKeyInput = key.Sanitise();
-            return false;
+            uint virtualKeyCode = (uint)key;
+            uint scanCode = MapVirtualKey(virtualKeyCode, 0);
+            IntPtr inputLocaleIdentifier = GetKeyboardLayout(0);
+
+            StringBuilder result = new StringBuilder();
+            ToUnicodeEx(virtualKeyCode, scanCode, keyboardState, result, (int)5, (uint)0, inputLocaleIdentifier);
+
+            return result.ToString();
         }
+
+        [DllImport("user32.dll")]
+        static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll")]
+        static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetKeyboardLayout(uint idThread);
+
+        [DllImport("user32.dll")]
+        static extern int ToUnicodeEx(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pwszBuff, int cchBuff, uint wFlags, IntPtr dwhkl);
+
+        public static string Sanitise(this Keys key)
+        {
+            return KeyCodeToUnicode(key);
+        }
+
+        public static string SanitiseLower(this Keys key)
+        {
+            return KeyCodeToUnicode(key, true);
+        }
+
     }
 }
