53d744f9d038b2884d3d522a601debeaa03299fb
Boris Fritscher <boris@fritscher.ch>
2017-07-16 21:31:38 +0200

    Visual circles arround mouse cursor and preferences menu

--- src/Carnac/UI/KeyShowViewModel.cs
+++ src/Carnac/UI/KeyShowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
 using Carnac.Logic;
 using Carnac.Logic.Models;
 
@@ -15,5 +16,12 @@ namespace Carnac.UI
         public ObservableCollection<Message> Messages { get; private set; }
 
         public PopupSettings Settings { get; set; }
+
+        public Point CursorPosition { get; set; }
+
+        public Thickness CursorMargins
+        {
+            get { return new Thickness(CursorPosition.X - 10, CursorPosition.Y - 10 , 0, 0); }
+        }
     }
 }
