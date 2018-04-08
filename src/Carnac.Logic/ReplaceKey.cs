9c10b10967a73a1c9b0aeff6d15af403f31ff860
Chris Cammack <clc1024@hotmail.com>
2018-04-07 20:23:01 -0700

    Remove accidental white space.

--- src/Carnac.Logic/ReplaceKey.cs
+++ src/Carnac.Logic/ReplaceKey.cs
@@ -102,7 +102,7 @@ namespace Carnac.Logic
 
         public static string Sanitise(this Keys key)
         {
-             return Replacements.ContainsKey(key) ? Replacements[key] : string.Format(key.ToString());
+            return Replacements.ContainsKey(key) ? Replacements[key] : string.Format(key.ToString());
         }
 
         public static bool SanitiseShift(this Keys key, out string sanitisedKeyInput)
