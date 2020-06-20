51f1fb3e9820651853fe71caf25897cbea2cb53a
Brendan Forster <github@brendanforster.com>
2020-06-20 13:28:00 -0300

    port Carnac to new project format

--- src/Carnac/Properties/AssemblyInfo.cs
+++ src/Carnac/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Carnac")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Carnac")]
-[assembly: AssemblyCopyright("Copyright © Code52 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
